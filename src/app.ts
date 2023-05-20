7f90fb53b049cf5b206de4fde0f59493a63bd209
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-19 22:40:35 -0300

    create schema User with ORM Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient() // create connection with database
+
+prisma.user.create({
+  data: {
+    name: 'Mateus Raimundo',
+    email: 'mateus@email.com',
+  },
+})
