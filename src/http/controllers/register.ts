8fb4b8abc36bb6c26129f91d3e307db38d0d1a2f
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-20 20:45:38 -0300

    transform password in hash

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -10,12 +11,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body) // parse do a 'throw' automatic of error
+  const password_hash = await hash(password, 6) // 6 rounds
+  const userWithSameEmail = await prisma.user.findUnique({
+    // 'findUnique' find just records uniques or primary keys
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send() // error for when exist data duplicated
+  }
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
