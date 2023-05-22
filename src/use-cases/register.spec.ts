dec83703ed409521a3829292689cbf22dce9663b
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-22 14:44:30 -0300

    create test password_hash

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+// import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    // const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    }) // here we need the repository that we are going to use - let's put a dummy repository so that this file has no relation to the repository external to this file - this is a test unitary
+
+    const { user } = await registerUseCase.execute({
+      name: 'Mateus',
+      email: 'mateusraimundo1995@email.com',
+      password: '123456',
+    })
+    const isPasswordCorrectlyHashed = await compare(
+      // let's compare the passwords to see if they are the same if hashed
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,26 +1,35 @@
 import { UsersRepository } from '@/repositories/users.repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
+interface RegisterUseCaseResponse {
+  // type of return of function RegisterUseCase
+  user: User
+}
 export class RegisterUseCase {
   // each class with use case will to unique method
   constructor(private usersRepository: UsersRepository) {} // we let's get our dependencies in this 'constructor' how parameter - 'private' is a key word for transform 'usersRepository' in a property in class
-
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6) // 6 rounds
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new UserAlreadyExistsError() // throw error
     }
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       email,
       name,
       password_hash,
     })
+    return { user }
   }
 }
