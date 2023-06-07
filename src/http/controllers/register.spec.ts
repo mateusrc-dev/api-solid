1f0033599e9041623e6b94c2911089af785e2c90
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-07 10:31:29 -0300

    create teste controller register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // ready is an event that fastify emits to know if the application has finished uploading the routes
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Mateus Raimundo',
+      email: 'mateus@email.com',
+      password: '123456',
+    }) // 'node' native server inside 'app', the fastify instance
+
+    expect(response.statusCode).toEqual(201) // test that verifies that the user was successfully created
+  })
+})
